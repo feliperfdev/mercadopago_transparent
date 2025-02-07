e95b7d9b636570fb1e44691dd702717dc0cf6972
Felipe Ribeiro <feliper.dev@gmail.com>
2025-02-06 21:45:05 -0300

    UTC

--- lib/src/payment/payment_repository.dart
+++ lib/src/payment/payment_repository.dart
@@ -1,4 +1,3 @@
-import 'package:intl/intl.dart';
 import 'package:mercadopago_transparent/src/payment/methodspayment_model.dart';
 import 'package:mercadopago_transparent/src/payment/payment_model.dart';
 import 'package:mercadopago_transparent/src/request_repository.dart';
@@ -145,16 +144,16 @@ class PaymentRepository {
     final data = {
       "transaction_amount": amount,
       "description": description ?? "",
-      "date_of_expiration": DateFormat('yyyy-MM-dd HH:mm:ss.SSSz')
-          .format(DateTime.now().add(
+      "date_of_expiration": DateTime.now()
+          .add(
             durationToExpires ??
                 const Duration(
                   hours: 23,
                   minutes: 59,
                   seconds: 59,
                 ),
-          ))
-          .replaceFirst(' ', 'T'),
+          )
+          .toUtc(),
       "payment_method_id": 'pix',
       "payer": payer
     };
