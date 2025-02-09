6699dd10437a78477413db8118b9fe112d0fca89
Felipe Ribeiro <feliper.dev@gmail.com>
2025-02-09 10:39:06 -0300

    items

--- lib/mercadopago_transparent.dart
+++ lib/mercadopago_transparent.dart
@@ -1,9 +1,9 @@
 library mercadopago_transparent;
 
-export 'src/mercadopago_repository.dart';
 export 'src/card/card_model.dart';
 export 'src/client/client_model.dart';
-export 'src/payment/payment_model.dart';
+export 'src/mercadopago_repository.dart';
 export 'src/payment/methodspayment_model.dart';
+export 'src/payment/payment_item.dart';
+export 'src/payment/payment_model.dart';
 export 'src/subscriptions/subscriptions_model.dart';
-
